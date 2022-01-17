d02348a2124fc490ae651abcdda561e736ffccb3
Pavel_Mitchenko <mitchenkopavel@gmail.com>
2022-01-17 11:08:29 +0300

    Fix private fields names

--- src/Multithreading/StructTearing/Program.cs
+++ src/Multithreading/StructTearing/Program.cs
@@ -38,18 +38,18 @@ namespace StructTearing
 
     struct LargeStruct
     {
-        private long A, B, C, D, E;
+        private long _a, _b, _c, _d, _e;
 
         public LargeStruct(long initialValue)
         {
-            A = B = C = D = E = initialValue;
+            _a = _b = _c = _d = _e = initialValue;
         }
 
-        public bool IsAllFieldsEqual => A == B && B == C && C == D;
+        public bool IsAllFieldsEqual => _a == _b && _b == _c && _c == _d;
 
         public override string ToString()
         {
-            return $" A = {A}\n B = {B}\n C = {C}\n D = {D}\n E = {E}\n";
+            return $" a = {_a}\n b = {_b}\n c = {_c}\n d = {_d}\n e = {_e}\n";
         }
     }
 }
